85c34a48da4759bf60418fe934984d17d6d594e3
faint <bunnyfaint@me.com>
2019-04-30 16:54:01 +0800

    完善版本
    1. first去重
    2. 增加idea配置文件
    3. 代码第一版完成
    4. 输出结果再result里

--- combine.py
+++ combine.py
@@ -1,3 +1,53 @@
-# coding: UTF-8
+# -*- coding: utf-8 -*-
+import random
 
-print("aa")
+
+def check_repeat(a):
+    for i in range(len(a)):
+        target = a[i]
+        for j in range(len(a)):
+            if target == a[j] and i != j:
+                print(str(i) + " = " + str(j) + " "+ target + " repeated")
+    print("checked")
+
+
+def read_txt(a):
+    file = open(a)
+    content = file.read()
+    file.close()
+    arr = content.splitlines()
+    check_repeat(arr)
+    return arr
+
+
+def get_new_random(a):
+    r = random.randint(0, len(a)-1)
+    return r
+
+
+def join_new(a,b):
+    random_a = get_new_random(a)
+    random_b = get_new_random(b)
+    return a[random_a] + b[random_b] + '\n'
+
+
+arr_family = read_txt('family.txt')
+arr_first = read_txt('first.txt')
+
+
+family_num = len(arr_family)
+first_num = len(arr_first)
+
+combine_array = []
+
+need = 5000
+while len(combine_array) < need:
+    new_name = join_new(arr_family, arr_first)
+    if new_name in combine_array:
+        continue
+    else:
+        combine_array.append(new_name)
+
+file = open('result.txt', 'w')
+file.writelines(combine_array)
+file.close()
